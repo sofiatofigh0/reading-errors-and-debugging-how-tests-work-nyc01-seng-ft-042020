319246aac4bee0f71723ed5cbb1e97fe688d3213
sofia tofigh <sst2140@columbia.edu>
2020-04-02 05:22:59 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,7 @@
+first_number = 5
+second_number= 6
+sum = first_number + second_number
+difference= first_number - second_number
+product= first_number * second_number
+quotient= first_number / second_number
 # Add your variables here
